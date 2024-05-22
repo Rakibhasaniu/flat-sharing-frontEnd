7b51a36e6d360c378f41487acda3c84fa42edec6
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-05-22 14:43:33 +0600

    navbar and footer created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
+"use client"
 import React from 'react';
 import Button from '@mui/material/Button';
+import Navbar from '@/components/Shared/Navbar/Navbar';
+import Footer from '@/components/Shared/Footer/Footer';
 
 const HomePage = () => {
   return (
     <div>
-      <h1>Home Page</h1>
-      <Button variant="contained">Contained</Button>
+      <Navbar />
+      <Footer />
 
     </div>
   );
